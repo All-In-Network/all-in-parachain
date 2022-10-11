113d4e5878047aca74364ce212d4690a5e95d5dc
Santiago Arboleda Londoño <monoprosito@gmail.com>
2022-10-10 23:23:20 -0500

    Initializes rpc node publication

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -2,9 +2,40 @@
 # This script is meant to be run on Unix/Linux based systems
 set -e
 
-echo "*** Start Substrate node template ***"
+echo "*** Starting All in Network node ***"
 
-cd $(dirname ${BASH_SOURCE[0]})/..
+# Evaluate current directory
+# ...
 
-docker-compose down --remove-orphans
-docker-compose run --rm --service-ports dev $@
+
+# Takes three parameters to associate with the certificate:
+#
+# - Application name
+# - Domain name
+# - Email
+#
+
+COMPOSE_PROJECT_NAME=$1
+DOMAIN=$2
+EMAIL=$3
+
+
+# Description
+echo COMPOSE_PROJECT_NAME=${COMPOSE_PROJECT_NAME} > .env
+echo DOMAIN=${DOMAIN} >> .env
+echo EMAIL=${EMAIL} >> .env
+
+
+# Phase 1
+docker compose -f ./docker-compose-initiate.yml up -d nginx
+docker compose -f ./docker-compose-initiate.yml up certbot
+docker compose -f ./docker-compose-initiate.yml down
+
+# some configurations for let's encrypt
+curl -L --create-dirs -o conf/rpc/letsencrypt/options-ssl-nginx.conf https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf
+
+openssl dhparam -out conf/rpc/letsencrypt/ssl-dhparams.pem 2048
+
+# Phase 2
+crontab ./conf/rpc/crontab/crontab.conf
+docker compose -f ./docker-compose.yml up -d
